f317fca596c1826f5a53a59c80df5bb74550168f
dengh2 <hang.deng2@dell.com>
2021-12-14 05:32:18 -0500

    rewise

---
+++ profile.py
@@ -0,0 +1,81 @@
+
+duration_6days_small_range = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.04,                            # 止盈利点 
+    "stop_loss": 0.02,                              # 止损点
+    "duration": 6,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
+duration_8days_small_range = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.04,                            # 止盈利点 
+    "stop_loss": 0.02,                              # 止损点
+    "duration": 8,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
+duration_10days_small_range = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.04,                            # 止盈利点 
+    "stop_loss": 0.02,                              # 止损点
+    "duration": 10,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
+duration_12days_small_range = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.04,                            # 止盈利点 
+    "stop_loss": 0.02,                              # 止损点
+    "duration": 12,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
+duration_6days_big_range = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.05,                            # 止盈利点 
+    "stop_loss": 0.03,                              # 止损点
+    "duration": 6,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
+duration_8days_big_range  = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.05,                            # 止盈利点 
+    "stop_loss": 0.03,                              # 止损点
+    "duration": 8,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
+duration_10days_big_range  = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.05,                            # 止盈利点 
+    "stop_loss": 0.03,                              # 止损点
+    "duration": 10,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
+duration_12days_big_range = {
+    "num_stock": 4,                                 # 最大选取股票数量
+    "tx_amount": 50000,                             # 单次下注本金
+    "tx_fee": 0.003,                                # 完成一次交易成本
+    "stop_profit": 0.05,                            # 止盈利点 
+    "stop_loss": 0.03,                              # 止损点
+    "duration": 12,                                  # 计划持有最大交易天数
+    "history_period": ['2010-01-01', '2021-12-30']  # 历史数据区间
+}
+
